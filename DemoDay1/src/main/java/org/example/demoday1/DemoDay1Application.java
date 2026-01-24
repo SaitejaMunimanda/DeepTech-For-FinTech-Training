1632145519c7b2fff7b0830e7f005eb61d84b660
Saiteja <saitejamunimanda@gmail.com>
2026-01-24 17:41:31 +0530

    Spring Boot Hello World application

---
+++ DemoDay1/src/main/java/org/example/demoday1/DemoDay1Application.java
@@ -0,0 +1,14 @@
+package org.example.demoday1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoDay1Application.class, args);
+        System.out.print("Hello World");
+    }
+
+}
---
+++ DemoDay1/src/test/java/org/example/demoday1/DemoDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demoday1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoDay1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
